19eebb33f061ee252fe619f6ec1cca770396ad48
Rajveer <rajveersinghranaofficial@gmail.com>
2025-03-16 16:20:58 +0530

    Project Initiated!!!

---
+++ app/src/main/java/com/rajveer/cultureconnect/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rajveer.cultureconnect
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.rajveer.cultureconnect.ui.theme.CultureConnectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CultureConnectTheme {
+            }
+        }
+    }
+}
